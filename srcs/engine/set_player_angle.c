b360b35f42bc13e2b959e83dfb01a3e9248b8a1a
Momox <morgane.geisler@gmail.com>
2024-01-06 12:28:04 +0100

    update

--- srcs/engine/set_player_angle.c
+++ srcs/engine/set_player_angle.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   set_player_angle.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edelarbr <edelarbr@student.42mulhouse.f    +#+  +:+       +#+        */
+/*   By: momox <momox@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 21:18:02 by edelarbr          #+#    #+#             */
-/*   Updated: 2024/01/04 19:29:31 by edelarbr         ###   ########.fr       */
+/*   Updated: 2024/01/06 12:26:50 by momox            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@ void	set_player_angle(t_player *player, char c)
 	if (c == 'E')
 		player->angle = 0;
 	else if (c == 'S')
-		player->angle = 3*PI/2;
+		player->angle = 3 * PI / 2;
 	else if (c == 'W')
 		player->angle = PI;
 	else if (c == 'N')
-		player->angle = PI/2;
+		player->angle = PI / 2;
 }
 
 // /* et t'en peux plus là ? T'as trop envie de chier ? chie toi dessus */
+// ?????? completement fou toi
