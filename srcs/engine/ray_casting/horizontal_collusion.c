53393d0b5fae3afe285a4b6f768d6c20627b8990
Momox <morgane.geisler@gmail.com>
2024-01-21 17:10:17 +0100

    segfault in horizontal when checking vertical right ??

--- srcs/engine/ray_casting/horizontal_collusion.c
+++ srcs/engine/ray_casting/horizontal_collusion.c
@@ -6,7 +6,7 @@
 /*   By: momox <momox@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 16:48:07 by edelarbr          #+#    #+#             */
-/*   Updated: 2024/01/21 03:45:45 by momox            ###   ########.fr       */
+/*   Updated: 2024/01/21 17:10:17 by momox            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,28 +46,25 @@ double	looking_left_down_h(t_ray *ray, t_map *gps)
 		ray->one_piece_x -= sqrt(pow(sqrt(pow(1, 2) + pow(1 / tan(ray->angle), 2)), 2) - pow(1, 2));
 		ray->one_piece_y += 1;
 	}
+	printf("\n\n");
 	return (ray->len);
 }
-//le côté droit ne fonctionne pas très bien (segfault)
+//le mur droit segfault
 double	looking_right_up_h(t_ray *ray, t_map *gps)
 {
-	printf("RIGHT UP\n\n");
+	printf("RIGHT UP\n");
 	printf("HORIZONTAL\n");
 	ray->len = (1 - (*ray->y - floor(*ray->y))) * sqrt(pow(1, 2) + pow(1 / tan(ray->angle), 2));
-	printf("angle = %f\n", ray->angle);
-	printf("len = %f\n\n", ray->len);
-	printf("x = %f et y = %f\n", *ray->x, *ray->y);
-	// ray->one_piece_x = *ray->x + sqrt(pow(ray->len, 2) - pow(*ray->y - floor(*ray->y), 2));
 	ray->one_piece_x = *ray->x + sqrt(pow(ray->len, 2) - pow((1 - (*ray->y - floor(*ray->y))), 2));
-	ray->one_piece_y = floor(*ray->y);
+	ray->one_piece_y = floor(*ray->y) - 1;
 	is_collision(gps, (int)floor(ray->one_piece_y), (int)floor(ray->one_piece_x));
 	while (!is_collision(gps, (int)floor(ray->one_piece_y), (int)floor(ray->one_piece_x)))
 	{
-		printf("onepiece y = %f\n onepiece x = %f\n", ray->one_piece_y, ray->one_piece_x);
 		ray->len += sqrt(pow(1, 2) + pow(1 / tan(ray->angle), 2));
 		ray->one_piece_x += sqrt(pow(sqrt(pow(1, 2) + pow(1 / tan(ray->angle), 2)), 2) - pow(1, 2));
 		ray->one_piece_y -= 1;
 	}
+	printf("\n\n");
 	return (ray->len);
 }
 
@@ -76,21 +73,16 @@ double	looking_right_down_h(t_ray *ray, t_map *gps)
 	printf("RIGHT DOWN\n");
 	printf("HORIZONTAL\n");
 	ray->len = (1 - (*ray->y - floor(*ray->y))) * sqrt(pow(1, 2) + pow(1 / tan(ray->angle), 2));
-	printf("angle = %f\n", ray->angle);
-	printf("len = %f\n\n", ray->len);
-	printf("x = %f et y = %f\n", *ray->x, *ray->y);
-	// ray->one_piece_x = *ray->x + sqrt(pow(ray->len, 2) - pow(*ray->y - floor(*ray->y), 2));
 	ray->one_piece_x = *ray->x + sqrt(pow(ray->len, 2) - pow((1 - (*ray->y - floor(*ray->y))), 2));
 	ray->one_piece_y = floor(*ray->y) + 1;
-	printf("x = %d et y = %d\n", (int)ray->one_piece_x, (int)ray->one_piece_y);
 	is_collision(gps, (int)floor(ray->one_piece_y), (int)floor(ray->one_piece_x));
 	while (!is_collision(gps, (int)floor(ray->one_piece_y), (int)floor(ray->one_piece_x)))
 	{
-		printf("onepiece y = %f\n onepiece x = %f\n", ray->one_piece_y, ray->one_piece_x);
 		ray->len += sqrt(pow(1, 2) + pow(1 / tan(ray->angle), 2));
 		ray->one_piece_x += sqrt(pow(sqrt(pow(1, 2) + pow(1 / tan(ray->angle), 2)), 2) - pow(1, 2));
 		ray->one_piece_y += 1;
 	}
+	printf("\n\n");
 	return (ray->len);
 }
 
