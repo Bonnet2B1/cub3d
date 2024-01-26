b7213e36a6a3d444c74c1630176768a0215b4c11
Eli <elias.delarbre@gmail.com>
2024-01-26 15:22:00 +0100

    Update player movement logic

--- srcs/events/player_movement.c
+++ srcs/events/player_movement.c
@@ -6,7 +6,7 @@
 /*   By: edelarbr <edelarbr@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 15:27:01 by edelarbr          #+#    #+#             */
-/*   Updated: 2023/12/28 20:45:49 by edelarbr         ###   ########.fr       */
+/*   Updated: 2024/01/26 15:21:44 by edelarbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,14 @@ void	move_forward(t_game_data *data)
 	t_player	*player;
 
 	player = data->player;
-	if (data->gps->map[(int)(player->y - STEP_LEN)][(int)player->x] == '1') // - '- STEP_LEN' à modifier lorqu'on prendra en compte l'angle de rotation du joueur
+	if (data->gps->map[(int)(player->y + STEP_LEN * sin(player->angle))][(int)(player->x + STEP_LEN * sin(player->angle))] != '0')
 		return ;
-	player->y -= STEP_LEN;
+	player->y += (STEP_LEN * sin(player->angle));
 	player->minimap_img->instances[0].y
 		= player->y * data->gps->minimap_img_size;
+	player->x += (STEP_LEN * cos(player->angle));
+	player->minimap_img->instances[0].x
+		= player->x * data->gps->minimap_img_size;
 }
 
 void	move_backward(t_game_data *data)
@@ -29,11 +32,14 @@ void	move_backward(t_game_data *data)
 	t_player	*player;
 
 	player = data->player;
-	if (data->gps->map[(int)(player->y + STEP_LEN)][(int)player->x] == '1') // - '+ STEP_LEN' à modifier lorqu'on prendra en compte l'angle de rotation du joueur
+	if (data->gps->map[(int)(player->y + STEP_LEN * sin(get_principal_measure(player->angle + PI)))][(int)(player->x + STEP_LEN * sin(get_principal_measure(player->angle + PI)))] != '0')
 		return ;
-	player->y += STEP_LEN;
+	player->y += (STEP_LEN * sin(get_principal_measure(player->angle + PI)));
 	player->minimap_img->instances[0].y
 		= player->y * data->gps->minimap_img_size;
+	player->x += (STEP_LEN * cos(get_principal_measure(player->angle + PI)));
+	player->minimap_img->instances[0].x
+		= player->x * data->gps->minimap_img_size;
 }
 
 void	move_left(t_game_data *data)
@@ -41,9 +47,12 @@ void	move_left(t_game_data *data)
 	t_player	*player;
 
 	player = data->player;
-	if (data->gps->map[(int)player->y][(int)(player->x - STEP_LEN)] == '1') // - '- STEP_LEN' à modifier lorqu'on prendra en compte l'angle de rotation du joueur
+	if (data->gps->map[(int)(player->y + STEP_LEN * sin(get_principal_measure(player->angle - PI / 2)))][(int)(player->x + STEP_LEN * sin(get_principal_measure(player->angle - PI / 2)))] != '0')
 		return ;
-	player->x -= STEP_LEN;
+	player->y += (STEP_LEN * sin(get_principal_measure(player->angle - PI / 2)));
+	player->minimap_img->instances[0].y
+		= player->y * data->gps->minimap_img_size;
+	player->x += (STEP_LEN * cos(get_principal_measure(player->angle - PI / 2)));
 	player->minimap_img->instances[0].x
 		= player->x * data->gps->minimap_img_size;
 }
@@ -53,34 +62,12 @@ void	move_right(t_game_data *data)
 	t_player	*player;
 
 	player = data->player;
-	if (data->gps->map[(int)(player->y)][(int)(player->x + STEP_LEN)] == '1') // - '+ STEP_LEN' à modifier lorqu'on prendra en compte l'angle de rotation du joueur
+	if (data->gps->map[(int)(player->y + STEP_LEN * sin(get_principal_measure(player->angle + PI / 2)))][(int)(player->x + STEP_LEN * sin(get_principal_measure(player->angle + PI / 2)))] != '0')
 		return ;
-	player->x += STEP_LEN;
+	player->y += (STEP_LEN * sin(get_principal_measure(player->angle + PI / 2)));
+	player->minimap_img->instances[0].y
+		= player->y * data->gps->minimap_img_size;
+	player->x += (STEP_LEN * cos(get_principal_measure(player->angle + PI / 2)));
 	player->minimap_img->instances[0].x
 		= player->x * data->gps->minimap_img_size;
 }
-
-// void	player_movement(t_game_data *data, int delta_x, int delta_y)
-// {
-// 	double	player_step;
-// 	int		wanted_x;
-// 	int		wanted_y;
-
-// 	player_step = 0.1;
-// 	wanted_x = data->gps->px + delta_x * player_step;
-// 	wanted_y = data->gps->py + delta_y * player_step;
-// 	if (wanted_x > 0 && data->gps->map[wanted_y][wanted_x + 1] == '1')
-// 		return ;
-// 	else if (wanted_y > 0 && data->gps->map[wanted_y + 1][wanted_x] == '1')
-// 		return ;
-// 	else if (data->gps->map[wanted_y][wanted_x] == '1')
-// 		return ;
-// 	data->gps->py += player_step * delta_y;
-// 	data->gps->px += player_step * delta_x;
-// 	// printf("Going in %f, %f (%d, %d)\n", data->gps->px, data->gps->py,
-// 	// 	(int)data->gps->px, (int)data->gps->py);
-// 	data->assets->minimap_player_img->instances[0].y
-// 		= data->gps->py * data->gps->minimap_img_size;
-// 	data->assets->minimap_player_img->instances[0].x
-// 		= data->gps->px * data->gps->minimap_img_size;
-// }
